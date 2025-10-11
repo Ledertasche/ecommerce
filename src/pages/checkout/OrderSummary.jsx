12bd8d5978c77ea70b126eeef95883f127186bf4
Stefan Bauer <bauersteff58@gmx.de>
2025-10-11 20:48:09 +0200

    delete cart-item

--- src/pages/checkout/OrderSummary.jsx
+++ src/pages/checkout/OrderSummary.jsx
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import dayjs from 'dayjs';
 import formatMoney from '../../utils/money';
 import DeliveryOptions from './DeliveryOptions';
@@ -10,6 +11,11 @@ function OrderSummary({ cart, deliveryOptions, loadCart }) {
               .find((deliveryOption) => {
                 return deliveryOption.id === cartItem.deliveryOptionId;
               });
+
+              const deleteCartItem = async () => {
+                await axios.delete(`/api/cart-items/${cartItem.productId}`);
+                await loadCart();
+              };
 
             return (
                 <div key={cartItem.productId} 
@@ -38,7 +44,9 @@ function OrderSummary({ cart, deliveryOptions, loadCart }) {
                   <span className="update-quantity-link link-primary">
                     Update
                   </span>
-                  <span className="delete-quantity-link link-primary">
+                  <span className="delete-quantity-link link-primary"
+                     onClick={deleteCartItem}>
+                   
                     Delete
                   </span>
                 </div>
